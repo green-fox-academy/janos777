d110bf44dfb3459ed4ca9b597d429f7a5ae70a0f
janos777 <garay.janos@gmail.com>
2018-02-19 17:40:14 +0100

    3 days in 1

---
+++ week-01/day-03/06 - CodingHours/CodingHours/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            //
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            //
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+
+            int dailyHours = 6;
+            int semesterLengthWeek = 17;
+            int semesterLengthWorkDays = semesterLengthWeek * 5;
+            double weeklyWorkHours = 52;
+            
+            
+            Console.WriteLine("An attendee is coding " + (semesterLengthWorkDays * dailyHours) + " hours in a semester.");
+            Console.WriteLine("Percentage of the coding hours in the semester is: " + (semesterLengthWeek * weeklyWorkHours * 100) / (semesterLengthWeek *7 * 24));
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ week-02/day-01/Matrix - other way/Matrix - other way/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+
+            int number = 4;
+
+            int[][] matrix = new int[number][];
+
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
