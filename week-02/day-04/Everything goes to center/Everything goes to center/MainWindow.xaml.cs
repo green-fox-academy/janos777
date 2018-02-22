9f7a381e7da35c9317db8af95862c2e5a4b86a10
janos777 <garay.janos@gmail.com>
2018-02-22 17:17:25 +0100

    week2 day4

--- week-02/day-04/Everything goes to center/Everything goes to center/MainWindow.xaml.cs
+++ week-02/day-04/Everything goes to center/Everything goes to center/MainWindow.xaml.cs
@@ -20,20 +20,55 @@ namespace Everything_goes_to_center
     /// </summary>
     public partial class MainWindow : Window
     {
+        int x = 0;
+        int y = 0;
+        int z = 20;
+        
+
         public MainWindow()
         {
             InitializeComponent();
             var foxDraw = new FoxDraw(canvas);
+
             // create a line drawing function that takes 2 parameters:
             // the x and y coordinates of the line's starting point
             // and draws a line from that point to the center of the canvas.
             // fill the canvas with lines from the edges, every 20 px, to the center.
 
+            canvas.Width = 600;
+            canvas.Height = 600;
 
+            for (x = 0; x <= canvas.Width; x += z)
+            {
+                if (x == 0 || x == canvas.Width)
+                {
+                    for (y = 0; y <= canvas.Height; y += z)
+                    {
+                        LineDrawing(x, y);
+                    }
+                }
 
+                if (0 < x && x < canvas.Width)
+                {
+                    y = 0;
+                    LineDrawing(x, y);
 
+                    y = Convert.ToInt32(canvas.Height);
+                    LineDrawing(x, y);
+                }
+            }
+        }
 
+        public void LineDrawing(int x, int y)
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
 
+            foxDraw.StrokeColor(Colors.Black);
+
+            var startPoint = new Point(x, y);
+            var endPoint = new Point(canvas.Width / 2, canvas.Height / 2);
+            foxDraw.DrawLine(startPoint, endPoint);
         }
     }
 }
---
+++ week-02/day-04/Starry night/Starry night/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Starry_night
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int squareSize = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            // Draw the night sky:
+            //  - The background should be black
+            //  - The stars can be small squares
+            //  - The stars should have random positions on the canvas
+            //  - The stars should have random color (some shade of grey)
+
+            canvas.Width = 600;
+            canvas.Height = 600;
+            canvas.Background = new SolidColorBrush(Colors.Black);
+
+            for (int i = 0; i < 20; i++)
+            {
+                Random r = new Random();
+                double x = r.Next(0, (Convert.ToInt32(canvas.Width)) - squareSize);
+
+                Thread.Sleep(100);
+                Random s = new Random();
+                double y = s.Next(0, (Convert.ToInt32(canvas.Height)) - squareSize);
+
+                int c = s.Next(0, 255);
+                byte cByte = Convert.ToByte(c);
+                Color color = new Color();
+                color = Color.FromRgb(cByte, cByte, cByte);
+
+                foxDraw.StrokeColor(color);
+                foxDraw.FillColor(color);
+                foxDraw.DrawRectangle(x, y, squareSize, squareSize);
+            }
+        }
+    }
+}
