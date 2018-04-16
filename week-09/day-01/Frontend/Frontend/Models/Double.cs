5e4a487d85b2f41ff49ca19fe4ef1609aba21123
janos777 <garay.janos@gmail.com>
2018-04-16 13:23:43 +0200

    added doubling

---
+++ week-09/day-01/Frontend/Frontend/Models/Double.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Frontend.Models
+{
+    public class Double
+    {
+        public int value;
+        public string content;
+
+        public Double(int value, string content)
+        {
+            this.value = value;
+            this.content = content;
+        }
+    }
+}
